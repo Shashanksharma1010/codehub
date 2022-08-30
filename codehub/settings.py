00cc5300d2be89619756b4ae465a9f5b1dc05bac
ishantsingh1010 <94222188+ishantsingh1010@users.noreply.github.com>
2022-08-30 17:32:16 +0530

    Update settings.py

--- codehub/settings.py
+++ codehub/settings.py
@@ -147,3 +147,11 @@ LOGIN_URL = 'members/login_user'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MIDDLEWARE = [
+...
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
